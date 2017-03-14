66f4723312eba71c7b385d9c83c135509cc5e0bc
joalruiz <jalb80@gmail.com>
2017-03-14 15:24:17 +0100

    Applying properties to donut-chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,13 @@ import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
 import {NgModule} from "@angular/core";
 import {AmChartsModule} from "amcharts3-angular2";
-import {ChartComponent} from "./util/components/donut-chart/donut-chart.component";
+import {DonutChartComponent} from "./util/components/donut-chart/donut-chart.component";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChartComponent
+    DonutChartComponent
   ],
   imports: [
     BrowserModule,
--- src/app/util/components/donut-chart/donut-chart.component.ts
+++ src/app/util/components/donut-chart/donut-chart.component.ts
@@ -1,86 +1,63 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'donut-chart',
   templateUrl: 'donut-chart.component.html'
 })
-export class ChartComponent implements OnInit {
-  private id = "chartdiv";
+export class DonutChartComponent implements OnInit {
+  @Input() chartData: Object;
+  private id: string;
 
   private options;
 
   constructor() {
-    console.log('Constructor....');
+    this.id = 'id';
   }
 
   ngOnInit() {
-    console.log('ngOnInit....');
-    var dataProvider = [];
-
-    // Generate random data
-    for (var year = 1950; year <= 2005; ++year) {
-      dataProvider.push({
-        year: "" + year,
-        value: Math.floor(Math.random() * 100) - 50
-      });
-    }
-
     this.options = {
-      "type": "serial",
-      "theme": "light",
-      "marginTop":0,
-      "marginRight": 80,
-      "dataProvider": dataProvider,
-      "valueAxes": [{
-        "axisAlpha": 0,
-        "position": "left"
-      }],
-      "graphs": [{
-        "id":"g1",
-        "balloonText": "[[category]]<br><b><span style='font-size:14px;'>[[value]]</span></b>",
-        "bullet": "round",
-        "bulletSize": 8,
-        "lineColor": "#d1655d",
-        "lineThickness": 2,
-        "negativeLineColor": "#637bb6",
-        "type": "smoothedLine",
-        "valueField": "value"
-      }],
-      "chartScrollbar": {
-        "graph":"g1",
-        "gridAlpha":0,
-        "color":"#888888",
-        "scrollbarHeight":55,
-        "backgroundAlpha":0,
-        "selectedBackgroundAlpha":0.1,
-        "selectedBackgroundColor":"#888888",
-        "graphFillAlpha":0,
-        "autoGridCount":true,
-        "selectedGraphFillAlpha":0,
-        "graphLineAlpha":0.2,
-        "graphLineColor":"#c2c2c2",
-        "selectedGraphLineColor":"#888888",
-        "selectedGraphLineAlpha":1
+      "type": "pie",
+      "balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>",
+      "innerRadius": "30%",
+      "labelText": "[[percents]]%",
+      "labelRadius": -50,
+      "addClassNames": true,
+      "color": "#FFFFFF",
+      "colors": [
+        "#ffd600",
+        "#13cec4",
+        "#db9e01"
+      ],
+      "titleField": "category",
+      "valueField": "column-1",
+      "allLabels": [],
+      "balloon": {
+        "borderColor": "#000000",
+        "borderThickness": 1,
+        "color": "#FFFFFF",
+        "fillColor": "#000000"
       },
-      "chartCursor": {
-        "categoryBalloonDateFormat": "YYYY",
-        "cursorAlpha": 0,
-        "valueLineEnabled":true,
-        "valueLineBalloonEnabled":true,
-        "valueLineAlpha":0.5,
-        "fullWidth":true
+      "legend": {
+        "enabled": true,
+        "align": "center",
+        "markerType": "circle",
+        "position": "top"
       },
-      "dataDateFormat": "YYYY",
-      "categoryField": "year",
-      "categoryAxis": {
-        "minPeriod": "YYYY",
-        "parseDates": true,
-        "minorGridAlpha": 0.1,
-        "minorGridEnabled": true
-      },
-      "export": {
-        "enabled": true
-      }
+      "titles": [],
+      "dataProvider": [
+        {
+          "category": "category 1",
+          "column-1": 8
+        },
+        {
+          "category": "category 2",
+          "column-1": 6
+        },
+        {
+          "category": "category 3",
+          "column-1": 2
+        }
+      ]
     };
   }
 
