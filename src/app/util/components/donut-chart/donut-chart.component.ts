90e72489f39763c24f640a26c9e2e5d3d26d2cf1
joalruiz <jalb80@gmail.com>
2017-03-14 16:29:59 +0100

    Formatting ballon text

--- src/app/util/components/donut-chart/donut-chart.component.ts
+++ src/app/util/components/donut-chart/donut-chart.component.ts
@@ -14,10 +14,31 @@ export class DonutChartComponent implements OnInit {
     this.id = 'id';
   }
 
+  private formatContent(graphDataItem:any) {
+    let funds:Object[] = graphDataItem.dataContext['column-2'];
+    let fundHTML:any = document.createElement('table');
+
+    for (let fund of funds) {
+      let fundTR = document.createElement('tr');
+      let fundNameTD = document.createElement('td');
+      fundNameTD.setAttribute('align', 'left');
+      let fundValueTD = document.createElement('td');
+      fundValueTD.setAttribute('align', 'right');
+
+      fundNameTD.innerHTML = fund['name'];
+      fundValueTD.innerHTML = fund['value'];
+      fundTR.appendChild(fundNameTD);
+      fundTR.appendChild(fundValueTD);
+      fundHTML.appendChild(fundTR);
+    }
+    return fundHTML.outerHTML;
+  }
+
   ngOnInit() {
     this.options = {
       "type": "pie",
-      "balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>",
+      "balloonText": "[[column-2]]",
+      "balloonFunction": this.formatContent,
       "innerRadius": "30%",
       "labelText": "[[percents]]%",
       "labelRadius": -50,
@@ -47,15 +68,33 @@ export class DonutChartComponent implements OnInit {
       "dataProvider": [
         {
           "category": "category 1",
-          "column-1": 8
+          "column-1": 8,
+          "column-2": [
+            { "name": "RENTA FIJA MONETARIOS", "value": "10%" },
+            { "name": "RENTA FIJA CORTO PLAZO", "value": "10%" },
+            { "name": "RENTA FIJA LARGO PLAZO", "value": "30%" },
+            { "name": "RENTA FIJA FLEXIBLE", "value": "10%" },
+          ]
         },
         {
           "category": "category 2",
-          "column-1": 6
+          "column-1": 6,
+          "column-2": [
+            { "name": "RENTA FIJA MONETARIOS", "value": "30%" },
+            { "name": "RENTA FIJA CORTO PLAZO", "value": "10%" },
+            { "name": "RENTA FIJA LARGO PLAZO", "value": "30%" },
+            { "name": "RENTA FIJA FLEXIBLE", "value": "10%" },
+          ]
         },
         {
           "category": "category 3",
-          "column-1": 2
+          "column-1": 2,
+          "column-2": [
+            { "name": "RENTA FIJA MONETARIOS", "value": "40%" },
+            { "name": "RENTA FIJA CORTO PLAZO", "value": "10%" },
+            { "name": "RENTA FIJA LARGO PLAZO", "value": "30%" },
+            { "name": "RENTA FIJA FLEXIBLE", "value": "10%" },
+          ]
         }
       ]
     };
