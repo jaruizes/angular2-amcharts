c85f66aea45a78875191ee4a05a512bcac74bf46
joalruiz <jalb80@gmail.com>
2017-03-30 11:53:39 +0200

    Created adapters to data from API to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,16 @@ import {FundsService} from "./services/funds.service";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  fundsChartData: Object[];
   lineChartData: Object[];
   donutChartData: Object[];
-  idDonut:string;
-  idLine:string;
-  customerInvestmentPortfolio:string;
+  idDonut: string;
+  idLine: string;
+  customerInvestmentPortfolio: string;
 
   constructor(private fundsService: FundsService) {
-    this.donutChartData = fundsService.getSimulationFunds();
-    this.lineChartData = fundsService.getProfitability();
+    this.adaptSimulationDataFromAPI();
+    this.adaptProfitabilityDataFromAPI();
+    //this.lineChartData = fundsService.getProfitability();
     this.idDonut = 'chart001';
     this.idLine = 'chart002';
     this.customerInvestmentPortfolio = 'Tu cartera';
@@ -24,6 +24,112 @@ export class AppComponent {
 
   rollOverSlice(e) {
     console.log('rollOverSlice recived!!!!');
-    console.log(e.data);
+    console.log(e);
+  }
+
+  /**
+   * Adapts data received from API to the right format to the donut chart component
+   */
+  adaptSimulationDataFromAPI() {
+    let dataAPI:Object[] = this.fundsService.getSimulationFunds();
+    let fixedRent: Object = {
+      'category': 'RENTA FIJA',
+      'total': 0,
+      'description': 'Se trata de fondos de inversión que invierten, exclusivamente, en activos financieros de deuda a corto, medio y largo plazo, emitidos por el Estado o por empresas o entidades publicas y privadas tales como Letras del tesoro, Pagares empresariales, Bonos y Obligaciones, etc.',
+      'composition': []
+    };
+    let variableRent: Object = {
+      'category': 'RENTA VARIABLE',
+      'total': 0,
+      'description': 'Se trata de fondos de inversión que invierten en una combinación de activos de renta variable y en activos de renta fija. Según sea la mayor o menor proporción de inversión en renta variable o de renta fija determina si el fondo es de renta variable mixta o es de renta fija mixta, respectivamente.',
+      'composition': []
+    };
+    let others: Object = {
+      'category': 'OTROS',
+      'total': 0,
+      'composition': []
+    };
+
+    let fixedRentUpdated: Object = dataAPI.filter(function (item) {return item['tipo'] == 'F';}).reduce(AppComponent.simulationReducer, fixedRent);
+    let variableRentUpdated: Object = dataAPI.filter(function (item) {return item['tipo'] == 'V';}).reduce(AppComponent.simulationReducer, variableRent);
+    let othersUpdated: Object = dataAPI.filter(function (item) {return item['tipo'] == 'O';}).reduce(AppComponent.simulationReducer, others);
+    this.donutChartData = [fixedRentUpdated, variableRentUpdated, othersUpdated];
+  }
+
+  /**
+   * Adapts data received from API to the right format to the line chart component
+   */
+  adaptProfitabilityDataFromAPI() {
+    let dataAPI:Object[] = this.fundsService.getProfitability();
+    this.lineChartData = dataAPI.reduce(AppComponent.profitabilityReducer, { 'profitability': []})['profitability'];
+
+    console.log(this.lineChartData);
+  }
+
+  /**
+   * Reducer for adapting simulation data received from the API to the data needed for the donut chart component
+   * @param previousValue
+   * @param currentValue
+   * @returns {{category: any, total: any, description: any, composition: Object[]}}
+   */
+  static simulationReducer(previousValue, currentValue) {
+    let composition: Object[] = previousValue['composition'];
+    composition.push({'name': currentValue['nombre'], 'value': currentValue['porcentajeParticipacion']});
+
+    return {
+      'category': previousValue['category'],
+      'total': previousValue['total'] + currentValue['porcentajeParticipacion'],
+      'description': previousValue['description'],
+      'composition': composition
+    };
+  }
+
+  /**
+   * Reducer for adapting simulation data received from the API to the data needed for the line chart component
+   * @param previousValue
+   * @param currentValue
+   * @returns {any}
+   */
+  static profitabilityReducer (previousValue, currentValue) {
+    let currentDate:string = AppComponent.formatDate(currentValue['fecha']);
+    let profitability:Object[] = previousValue['profitability'];
+
+    // Search for the object with date equals to currentValue.date. Filter method must return an array with one element
+    // or empty if it doesn't exist yet
+    let index:number = -1;
+    let dateObjectArray:Object[] = profitability.filter(function(item, itemIndex) {
+      if (item['date'] == currentDate) {
+        index = itemIndex;
+        return true;
+      }
+      return false;
+    });
+
+    // If it doesn't exit yet, a new item with the current date is created and pushed into the profitability array
+    let item:Object = dateObjectArray[0];
+    if (index < 0) {
+      item = { 'date': currentDate };
+      profitability.push(item);
+    }
+
+    // In both cases, we set the new profitability record associated to the current date
+    item[currentValue['nombre']] = currentValue['valor'];
+    previousValue['profitability'] = profitability;
+
+    return previousValue;
+
+  }
+
+  static formatDate(date) {
+    let monthNames = [
+      'ENE', 'FEB', 'MAR',
+      'ABR', 'MAY', 'JUN', 'JUL',
+      'AGO', 'SEP', 'OCT',
+      'NOV', 'DIC'
+    ];
+
+    let year = date.getFullYear().toString().substr(-2);
+
+    return monthNames[date.getMonth()] + '-' + year;
   }
 }
--- src/app/services/funds.service.ts
+++ src/app/services/funds.service.ts
@@ -7,59 +7,90 @@ export class FundsService {
   }
 
   getSimulationFunds(): Object[] {
-    return [
+    /*alias required string
+    area required string
+    descripcion required string
+    divisa required string
+    nombre required string
+    porcentajeParticipacion required number
+    tipo required, one of (F, V, O) string*/
+
+    let apiResponse:Object[] = [
       {
-        'category': 'RENTA FIJA',
-        'total': 60,
-        'description': 'Se trata de fondos de inversión que invierten, exclusivamente, en activos financieros de deuda a corto, medio y largo plazo, emitidos por el Estado o por empresas o entidades publicas y privadas tales como Letras del tesoro, Pagares empresariales, Bonos y Obligaciones, etc.',
-        'composition': [
-          {'name': 'RENTA FIJA MONETARIOS', 'value': '10%'},
-          {'name': 'RENTA FIJA CORTO PLAZO', 'value': '10%'},
-          {'name': 'RENTA FIJA LARGO PLAZO', 'value': '30%'},
-          {'name': 'RENTA FIJA FLEXIBLE', 'value': '10%'},
-        ]
+        'nombre':'RENTA FIJA MONETARIOS',
+        'porcentajeParticipacion':10,
+        'tipo':'F',
       },
       {
-        'category': 'RENTA VARIABLE',
-        'total': 30,
-        'description': 'Se trata de fondos de inversión que invierten en una combinación de activos de renta variable y en activos de renta fija. Según sea la mayor o menor proporción de inversión en renta variable o de renta fija determina si el fondo es de renta variable mixta o es de renta fija mixta, respectivamente.',
-        'composition': [
-          {'name': 'RENTA VARIABLE A', 'value': '20%'},
-          {'name': 'RENTA VARIABLE B', 'value': '10%'},
-        ]
+        'nombre':'RENTA FIJA CORTO PLAZO',
+        'porcentajeParticipacion':10,
+        'tipo':'F',
       },
       {
-        'category': 'OTROS',
-        'total': 10,
-        'composition': [
-          {'name': 'MIXTO A', 'value': '5%'},
-          {'name': 'INMOBILIARIO ESPECIAL', 'value': '2.5%'},
-          {'name': 'GARANTIZADO BKT', 'value': '2.5%'}
-        ]
-      }
+        'nombre':'RENTA FIJA LARGO PLAZO',
+        'porcentajeParticipacion':30,
+        'tipo':'F',
+      },
+      {
+        'nombre':'RENTA FIJA FLEXIBLE',
+        'porcentajeParticipacion':10,
+        'tipo':'F',
+      },
+      {
+        'nombre':'RENTA VARIABLE A',
+        'porcentajeParticipacion':20,
+        'tipo':'V',
+      },
+      {
+        'nombre':'RENTA VARIABLE B',
+        'porcentajeParticipacion':10,
+        'tipo':'V',
+      },
+      {
+        'nombre':'MIXTO A',
+        'porcentajeParticipacion':5,
+        'tipo':'O',
+      },
+      {
+        'nombre':'INMOBILIARIO ESPECIAL',
+        'porcentajeParticipacion':2.5,
+        'tipo':'O',
+      },
+      {
+        'nombre':'GARANTIZADO BKT',
+        'porcentajeParticipacion':2.5,
+        'tipo':'O',
+      },
     ];
+
+    return apiResponse;
   }
 
   getProfitability(): Object[] {
-    let res: Object[] = [];
+    let dates:Date[] = [];
     for (let _i = 0; _i < 10; _i++) {
-      let month: number = 7 + _i;
-      let date: string = month <= 9 ? '12/200' + month : '12/20' + month;
-
-      let item: Object = {
-        'date': date,
-        'Tu cartera': FundsService.getRandomArbitrary(),
-        'IBEX': FundsService.getRandomArbitrary(),
-        'EUROSTOXX 50': FundsService.getRandomArbitrary(),
-        'S&P 500': FundsService.getRandomArbitrary(),
-        'Nikkei 225': FundsService.getRandomArbitrary(),
-        'MSCI World': FundsService.getRandomArbitrary()
-      };
-
-      res.push(item);
+      dates.push(new Date(2007 + _i,11,31));
     }
 
-    return res;
+    let apiResponse:Object = dates.reduce(function (previousValue, currentValue) {
+      let values:Object[] = previousValue['values'];
+      values.push(FundsService.buildSimpleValue(currentValue, 'Tu cartera'));
+      values.push(FundsService.buildSimpleValue(currentValue, 'IBEX'));
+      values.push(FundsService.buildSimpleValue(currentValue, 'EUROSTOXX 50'));
+      values.push(FundsService.buildSimpleValue(currentValue, 'S&P 500'));
+      values.push(FundsService.buildSimpleValue(currentValue, 'Nikkei 225'));
+      values.push(FundsService.buildSimpleValue(currentValue, 'MSCI World'));
+
+      return {
+        'values' : values
+      }
+
+    },{ 'values': []});
+
+
+    console.log(apiResponse);
+
+    return apiResponse['values'];
   }
 
   static getRandomArbitrary(): string {
@@ -68,4 +99,13 @@ export class FundsService {
     return (Math.random() * (max - min) + min).toFixed(2);
   }
 
+  static buildSimpleValue(date, name) {
+    return {
+      'fecha': date,
+      'nombre': name,
+      'tipo': name == 'Tu cartera' ? 'C' : 'I',
+      'valor': FundsService.getRandomArbitrary()
+    }
+  }
 }
+
